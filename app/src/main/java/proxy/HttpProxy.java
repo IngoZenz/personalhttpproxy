d8971a98fcc71865b8db966d7abbec54cbd1209d
IngoZenz <i.z@gmx.net>
2024-03-11 16:37:17 +0100

    switch to target version 25 in order to support android 14

--- app/src/main/java/proxy/HttpProxy.java
+++ app/src/main/java/proxy/HttpProxy.java
@@ -57,7 +57,7 @@ import util.LoggerInterface;
 
 public class HttpProxy implements Runnable, LoggerInterface
 {
-	public static final String VERSION = "1.57.5";
+	public static final String VERSION = "1.57.6-dev";
 	static public boolean debug;
 	static public String WORKDIR = "";	
 	private static String filterReloadURL;
